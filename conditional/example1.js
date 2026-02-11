874377baa88cade44ec7e47473aa68dbdf02e4bb
surekha <surekhagopidesi@gmail.com>
2026-02-11 11:14:34 +0530

    js files

---
+++ conditional/example1.js
@@ -0,0 +1,15 @@
+var color=prompt("Enter color");
+if(color=="red"){
+    console.log("stop");
+}
+else if(color=="yellow"){
+    console.log("Get Ready");
+}
+else if(color=="green"){
+    console.log("Go");
+    
+}
+else{
+    console.log("invalid color");
+    
+}
